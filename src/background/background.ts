c5e6a56e76774265b65d1bb51ba231a190ad4603
Seamus Finlayson <77430559+SeamusFinlayson@users.noreply.github.com>
2024-04-12 19:25:04 -0700

    Version 1.7.6 * Fix bug where full health bar is visible to players * Fix bug where making a token hidden would still show the name tag background * Add correct height for context menu embed depending on name tag and user roll * Update package dependencies * Improve name tag positioning when the origin above token is enabled

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,8 +1,5 @@
 import OBR from "@owlbear-rodeo/sdk";
-import { getPluginId } from "../getPluginId";
-import { startStatAttachments } from "./statAttachments";
-
-const menuIcon = new URL("../status.svg#icon", import.meta.url).toString();
+import startBackground from "./statAttachments";
 
 /**
  * This file represents the background script run when the plugin loads.
@@ -10,97 +7,14 @@ const menuIcon = new URL("../status.svg#icon", import.meta.url).toString();
  */
 
 OBR.onReady(async () => {
+  printVersionToConsole();
+  startBackground();
+});
+
+function printVersionToConsole() {
   fetch("/manifest.json")
     .then((response) => response.json())
     .then((json) =>
       console.log(json["name"] + " - version: " + json["version"]),
     );
-
-  //create player context menu icon
-  OBR.contextMenu.create({
-    id: getPluginId("player-menu"),
-    icons: [
-      {
-        icon: menuIcon,
-        label: "Edit Stats",
-        filter: {
-          every: [
-            { key: "type", value: "IMAGE" },
-            { key: "layer", value: "CHARACTER", coordinator: "||" },
-            { key: "layer", value: "MOUNT", coordinator: "||" },
-            { key: "layer", value: "PROP" },
-            {
-              key: [
-                "metadata",
-                "com.owlbear-rodeo-bubbles-extension/metadata",
-                "hide",
-              ],
-              value: true,
-              operator: "!=",
-            },
-          ],
-          permissions: ["UPDATE"],
-          roles: ["PLAYER"],
-          max: 1,
-        },
-      },
-    ],
-    shortcut: "Shift + S",
-    embed: {
-      url: "/src/edit-stats/editStats.html",
-      height: 126,
-    },
-  });
-
-  //create GM context menu icon
-  OBR.contextMenu.create({
-    id: getPluginId("gm-menu"),
-    icons: [
-      {
-        icon: menuIcon,
-        label: "Edit Stats",
-        filter: {
-          every: [
-            { key: "type", value: "IMAGE" },
-            { key: "layer", value: "CHARACTER", coordinator: "||" },
-            { key: "layer", value: "MOUNT", coordinator: "||" },
-            { key: "layer", value: "PROP" },
-          ],
-          roles: ["GM"],
-          max: 1,
-        },
-      },
-    ],
-    shortcut: "Shift + S",
-    embed: {
-      url: "/src/edit-stats/editStats.html",
-      height: 176,
-    },
-  });
-
-  OBR.contextMenu.create({
-    id: getPluginId("damage-tool"),
-    icons: [
-      {
-        icon: menuIcon,
-        label: "AOE Tool",
-        filter: {
-          every: [],
-          roles: ["GM"],
-        },
-      },
-    ],
-    shortcut: "Shift + S",
-    onClick(_, elementId) {
-      OBR.popover.open({
-        id: getPluginId("damage-tool-popover"),
-        url: "/src/damage-tool/damageTool.html",
-        height: 522,
-        width: 600,
-        anchorElementId: elementId,
-      });
-    },
-  });
-
-  startStatAttachments();
-});
+}
---
+++ src/background/contextMenuItems.ts
@@ -0,0 +1,111 @@
+import OBR from "@owlbear-rodeo/sdk";
+import { getPluginId } from "../getPluginId";
+import { Settings } from "./getGlobalSettings";
+export const menuIcon = new URL(
+  "../status.svg#icon",
+  import.meta.url,
+).toString();
+
+const NAME_HEIGHT = 40;
+const STATS_HEIGHT = 84;
+const HIDE_HEIGHT = 50;
+const BOTTOM_PADDING = 2;
+
+export default function createContextMenuItems(settings: Settings) {
+  let menuHeight = STATS_HEIGHT + BOTTOM_PADDING;
+  if (settings.nameTags) menuHeight += NAME_HEIGHT;
+
+  createPlayerMenu(menuHeight);
+  createGmMenu(menuHeight + HIDE_HEIGHT);
+  createDamageToolContextItem();
+}
+
+function createPlayerMenu(playerMenuHeight: number) {
+  OBR.contextMenu.create({
+    id: getPluginId("player-menu"),
+    icons: [
+      {
+        icon: menuIcon,
+        label: "Edit Stats",
+        filter: {
+          every: [
+            { key: "type", value: "IMAGE" },
+            { key: "layer", value: "CHARACTER", coordinator: "||" },
+            { key: "layer", value: "MOUNT", coordinator: "||" },
+            { key: "layer", value: "PROP" },
+            {
+              key: [
+                "metadata",
+                "com.owlbear-rodeo-bubbles-extension/metadata",
+                "hide",
+              ],
+              value: true,
+              operator: "!=",
+            },
+          ],
+          permissions: ["UPDATE"],
+          roles: ["PLAYER"],
+          max: 1,
+        },
+      },
+    ],
+    shortcut: "Shift + S",
+    embed: {
+      url: "/src/edit-stats/editStats.html",
+      height: playerMenuHeight,
+    },
+  });
+}
+
+function createGmMenu(gmMenuHeight: number) {
+  OBR.contextMenu.create({
+    id: getPluginId("gm-menu"),
+    icons: [
+      {
+        icon: menuIcon,
+        label: "Edit Stats",
+        filter: {
+          every: [
+            { key: "type", value: "IMAGE" },
+            { key: "layer", value: "CHARACTER", coordinator: "||" },
+            { key: "layer", value: "MOUNT", coordinator: "||" },
+            { key: "layer", value: "PROP" },
+          ],
+          roles: ["GM"],
+          max: 1,
+        },
+      },
+    ],
+    shortcut: "Shift + S",
+    embed: {
+      url: "/src/edit-stats/editStats.html",
+      height: gmMenuHeight,
+    },
+  });
+}
+
+function createDamageToolContextItem() {
+  OBR.contextMenu.create({
+    id: getPluginId("damage-tool"),
+    icons: [
+      {
+        icon: menuIcon,
+        label: "AOE Tool",
+        filter: {
+          every: [],
+          roles: ["GM"],
+        },
+      },
+    ],
+    shortcut: "Shift + S",
+    onClick(_, elementId) {
+      OBR.popover.open({
+        id: getPluginId("damage-tool-popover"),
+        url: "/src/damage-tool/damageTool.html",
+        height: 522,
+        width: 600,
+        anchorElementId: elementId,
+      });
+    },
+  });
+}
